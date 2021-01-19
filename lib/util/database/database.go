ca77ab6d9bd027108998101bcabdfa8aff154443
Raditya Pratama <radityaapratamaa@gmail.com>
2021-01-20 01:17:26 +0700

    database library

---
+++ lib/util/database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"article-test/domain/models"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+func Connect(cfg *models.Config) (*models.Database, error) {
+
+	masterDB, err := connectDB(&cfg.Databases.Master)
+	if err != nil {
+		return nil, errors.Wrap(err, "lib.util.database.ConnectMaster")
+	}
+
+	slaveDB, err := connectDB(&cfg.Databases.Slave)
+	if err != nil {
+		return nil, errors.Wrap(err, "lib.util.database.ConnectSlave")
+	}
+
+	database := &models.Database{
+		Master: masterDB,
+		Slave:  slaveDB,
+	}
+	return database, nil
+}
+
+func connectDB(cfg *models.DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Connect(cfg.Driver, cfg.ConnString)
+	if err != nil {
+		return nil, errors.Wrap(err, "lib.util.database.Connect")
+	}
+
+	maxLifetime := time.Duration(cfg.ConnMaxLifetime) * time.Second
+	db.SetConnMaxLifetime(maxLifetime)
+	maxIddleTime := time.Duration(cfg.ConnMaxIddleTime) * time.Second
+	db.SetConnMaxIdleTime(maxIddleTime)
+
+	db.SetMaxOpenConns(cfg.ConnMaxLifetime)
+	db.SetMaxIdleConns(cfg.MaxOpenConn)
+	return db, nil
+}
