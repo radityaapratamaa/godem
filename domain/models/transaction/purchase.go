671924a8008e5f033053c92465c39c2fde0e0463
Raditya Pratama <radityaapratamaa@gmail.com>
2021-05-25 01:25:00 +0700

    adding total price field

--- domain/models/transaction/purchase.go
+++ domain/models/transaction/purchase.go
@@ -5,18 +5,20 @@ import (
 )
 
 type Purchase struct {
-	ID           int64  `json:"id"`
-	GoodsID      int64  `json:"goods_id"`
-	Qty          int    `json:"qty"`
-	PurchaseDate string `json:"purchase_date"`
-	CreatedAt    string `json:"created_at"`
-	UpdatedAt    string `json:"udpated_at"`
-	DeletedAt    string `json:"deleted_at"`
+	ID           int64   `json:"id"`
+	GoodsID      int64   `json:"goods_id"`
+	Qty          int     `json:"qty"`
+	PurchaseDate string  `json:"purchase_date"`
+	TotalPrice   float64 `json:"total_price"`
+	CreatedAt    string  `json:"created_at"`
+	UpdatedAt    string  `json:"udpated_at"`
+	DeletedAt    string  `json:"deleted_at"`
 }
 
 type PurchaseResponse struct {
 	models.CUDResponse
-	Message string `json:"message"`
+	Message          string  `json:"message"`
+	TotalTransaction float64 `json:"total"`
 }
 
 type PurchaseRequest struct {
