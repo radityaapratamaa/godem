2bdcef64475155fd86f891a58ed89dfbf1e54d36
Raditya Pratama <radityaapratamaa@gmail.com>
2021-06-02 00:53:36 +0700

    register check token routes

--- infrastructure/service/_internal/http/api/routes.go
+++ infrastructure/service/_internal/http/api/routes.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"godem/infrastructure/middleware"
+	"godem/infrastructure/service/_internal/http/api/jwt"
 	"godem/infrastructure/service/_internal/http/api/user"
 	"godem/lib/util/response"
 )
@@ -13,16 +15,19 @@ import (
 type Routes struct {
 	router  *chi.Mux
 	modules *ModuleHandler
+	auth    *middleware.Auth
 }
 
 type ModuleHandler struct {
 	User *user.Handler
+	JWT  *jwt.Handler
 }
 
-func NewHandler(modules *ModuleHandler) *Routes {
+func NewHandler(modules *ModuleHandler, authHandler *middleware.Auth) *Routes {
 	return &Routes{
 		router:  chi.NewRouter(),
 		modules: modules,
+		auth:    authHandler,
 	}
 }
 
@@ -30,15 +35,18 @@ func (h *Routes) RegisterAndStartServer() error {
 
 	//register your routes here
 	h.router.Get("/ping", h.Ping)
+	h.router.Post("/login", h.modules.User.Login().Authenticate)
 
 	h.router.Route("/user", func(user chi.Router) {
+		user.Use(h.auth.JWT().ValidateGroup)
 		user.Get("/", h.modules.User.Master().GetList)
 		user.Get("/{id}", h.modules.User.Master().GetDetailByID)
 		user.Post("/", h.modules.User.Master().CreateNew)
 		user.Patch("/{id}", h.modules.User.Master().UpdateData)
 		user.Delete("/{id}", h.modules.User.Master().DeleteData)
-
 	})
+
+	h.router.Get("/token", h.auth.JWT().Validate(h.modules.JWT.Claim().GetJWTClaim))
 
 	log.Println("http listening on port :10000")
 	return http.ListenAndServe(":10000", h.router)
